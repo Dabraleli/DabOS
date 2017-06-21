19831d914ca778afd3c55f59ab3b1ed18488b960
Dabraleli <dabraleliid@gmail.com>
2017-06-21 20:26:40 +0500

    Fix #3

--- installer/installer.lua
+++ installer/installer.lua
@@ -1,7 +1,7 @@
 local fs = require("filesystem")
-
+ 
 print("Клонирование репозитория...")
-
+ 
 local internet=require("internet")
 local text=require("text")
 local filesystem=require("filesystem")
@@ -9,15 +9,16 @@ local unicode=require("unicode")
 local term=require("term")
 local event=require("event")
 local keyboard=require("keyboard")
-
+ 
 local repo,target
-
+ 
 repo="Dabraleli/DabOS"
-
+ 
 target="/"
-
+ 
 local function gitContents(repo,dir)
   local url="https://api.github.com/repos/"..repo.."/contents"..dir
+  print(url)
   dir = dir .. "/"
   local raw=""
   local files={}
@@ -27,11 +28,11 @@ local function gitContents(repo,dir)
   end
   raw=raw:gsub("%[","{"):gsub("%]","}"):gsub("(\".-\"):(.-[,{}])",function(a,b) return "["..a.."]="..b end)
   local t=load("return "..raw)()
-
+ 
   for i=1,#t do
     if t[i].type=="dir" then
       table.insert(directories,dir..t[i].name)
-
+ 
       local subfiles,subdirs=gitContents(repo,dir..t[i].name)
       for i=1,#subfiles do
         table.insert(files,subfiles[i])
@@ -45,37 +46,49 @@ local function gitContents(repo,dir)
   end
   return files, directories
 end
-
+ 
+ 
+print("Проверка лимита")
+local url = "https://api.github.com/rate_limit"
+raw = ""
+for chunk in internet.request(url) do
+  raw = raw..chunk
+end
+raw=raw:gsub("%[","{"):gsub("%]","}"):gsub("(\".-\"):(.-[,{}])",function(a,b) return "["..a.."]="..b end)
+local t=load("return "..raw)()
+print(t.resources.rate.remaining)
+ 
+ 
 local files,dirs=gitContents(repo,"/OS")
 print("Чтение директорий")
-
+ 
 for i=1,#dirs do
-  print("Создание директории "..dirs[i])
-  if filesystem.exists(dirs[i]) then
-    if not filesystem.isDirectory(dirs[i]) then
-      print("Ошибка: директория "..dirs[i].." блокируется файлом с тем же именем")
+  print("Создание директории "..string.sub(dirs[i], 4))
+  if filesystem.exists(string.sub(dirs[i], 4)) then
+    if not filesystem.isDirectory(string.sub(dirs[i], 4)) then
+      print("Ошибка: директория "..string.sub(dirs[i], 4).." блокируется файлом с тем же именем")
       return
     end
   else
-    filesystem.makeDirectory(dirs[i])
+    filesystem.makeDirectory(string.sub(dirs[i], 4))
   end
 end
-
+ 
 for i=1,#files do
   local replace=nil
-  if filesystem.exists(target..files[i]) then
-      filesystem.remove(target..files[i])
+  if filesystem.exists(string.sub(files[i], 4)) then
+      filesystem.remove(string.sub(files[i], 4))
   end
-  print("Загрузка "..files[i])
+  print("Загрузка "..string.sub(files[i], 4))
   local url="https://raw.github.com/"..repo.."/master"..files[i]
   local result,response=pcall(internet.request,url)
   if result then
     local raw=""
-    for chunk in response do       
-    	raw=raw..chunk
+    for chunk in response do      
+      raw=raw..chunk
     end
-    print("Сохранение "..files[i])
-    local file=io.open(files[i],"w")
+    print("Сохранение "..string.sub(files[i], 4))
+    local file=io.open(string.sub(files[i], 4),"w")
     file:write(raw)
     file:close()
   else
@@ -85,3 +98,4 @@ end
 print("ОС установлена")
 print("Через секунду компьютер будет перезагружен")
 os.sleep(1)
+computer.shutdown(true)
